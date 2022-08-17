c72eaf5b18a2d9d2527591b03b163322ab167aef
F3r <fdogm@outlook.com>
2022-08-17 00:35:27 -0500

    Prueba IS GLEZ

---
+++ src/pruebaisglez/PruebaISGlez.java
@@ -0,0 +1,15 @@
+
+package pruebaisglez;
+
+
+public class PruebaISGlez {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Prueba GITHUB GLEZ");
+               
+    }
+    
+}
